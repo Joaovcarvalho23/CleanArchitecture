2ce9405cd52158e6b0ed74722d01639b3b6d02b5
Joao <joaovictorcordeiro00@gmail.com>
2023-09-14 10:09:37 -0300

    Adicionar arquivos de projeto.

---
+++ Domain/Interfaces/IBaseRepository.cs
@@ -0,0 +1,18 @@
+﻿using CleanArchitecture.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Domain.Interfaces
+{
+    public interface IBaseRepository <Tipo> where Tipo : BaseEntity
+    {
+        void Create(Tipo entity);
+        void Update(Tipo entity);
+        void Delete(Tipo entity);
+        Task<Tipo> Get(Guid id, CancellationToken cancellationToken);
+        Task<List<Tipo>> GetAll(CancellationToken cancellationToken);
+    }
+}
