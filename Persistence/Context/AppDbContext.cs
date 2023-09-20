9520f419f05dd10526874a412745695a3c94dd9c
Joao <joaovictorcordeiro00@gmail.com>
2023-09-20 10:39:41 -0300

    Criando método CreateDatabase

--- Persistence/Context/AppDbContext.cs
+++ Persistence/Context/AppDbContext.cs
@@ -1,16 +1,11 @@
 ﻿using CleanArchitecture.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CleanArchitecture.Persistence.Context
+namespace CleanArchitecture.Persistence.Context;
+
+public class AppDbContext : DbContext
 {
-    public class AppDbContext : DbContext
-    {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-        public DbSet<User> Users { get; set; }
-    }
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    { }
+    public DbSet<User> Users { get; set; }
 }
